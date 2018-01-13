bdd7b3c6559f7a1be8ca578eaafc1245802e7aeb
frglezd <ragodrone@openmailbox.org>
2018-01-12 20:13:43 -0700

    Initial Commit

---
+++ Data structures/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Data structures
+//
+//  Created by Francisco Gonzalez  on 1/12/18.
+//  Copyright © 2018 Francisco  Gonzalez . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
